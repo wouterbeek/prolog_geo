8124d37f25eb1b034afccc0d70cf377fc10ab1f7
Wouter Beek <wouter@triply.cc>
2018-02-20 08:34:43 +0100

    DOC: Updated Prolog Pack info.

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 home('https://github.com/wouterbeek/Pro-GIS').
-keywords([geo,gis,wkt]).
+keywords(['geo-spatial','GIS']).
 name('Pro-GIS').
-title('Pro-GIS').
+title('Prolog-based GIS library (wrapper around GEOS)').
 version('0.0.1').
