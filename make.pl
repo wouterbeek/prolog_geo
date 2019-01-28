b7863e3643083724b8af8da855fcc48e878e1e0a
Wouter Beek <wouter@triply.cc>
2019-01-28 14:16:32 +0100

    FIXED: Export environment variables before running make.

--- make.pl
+++ make.pl
@@ -1,5 +1,6 @@
 :- use_module(library(filesex)).
 :- use_module(library(lists)).
+:- use_module(library(process)).
 :- use_module(library(prolog_pack), []).
 
 make :-
@@ -12,18 +13,18 @@ make :-
   post_install_foreign(Dir).
 
 make(Dir, Targets) :-
-  directory_file_path(Dir, 'Makefile', File),
-  exists_file(File), !,
+  absolute_file_name('Makefile', _, [access(read),relative_to(Dir)]),
   prolog_pack:build_environment(Env),
   Options = [directory(Dir),env(Env)],
   forall(
     member(Target, Targets),
     prolog_pack:run_process(path(make), [Target], Options)
   ).
-make(_, _).
 
 post_install_foreign(Dir) :-
   prolog_pack:setup_path,
   prolog_pack:save_build_environment(Dir),
+  absolute_file_name('buildenv.sh', File, [access(read),relative_to(Dir)]),
+  process_create(path(sh), [file(File)], []),
   prolog_pack:configure_foreign(Dir, []),
   prolog_pack:make_foreign(Dir, []).
